64472a11a58e343c032ecc92bcbfb4cccf9c5081
Adarsh <140302686+Aadarsh6@users.noreply.github.com>
2025-08-09 22:02:05 +0530

    dfj

--- src/rtc.ts
+++ src/rtc.ts
@@ -203,7 +203,7 @@ function handleJoinRoom(socket:WebSocket, payload:any, connectionId:string){
     // Send welcome message to the user
     socket.send(JSON.stringify({
         type: "system",
-        message: `Welcome to room ${roomId}. ${room?.users.length} total users including you.`
+        message: `${room?.users.length} total users including you.`
     }));
 
 
